8b89e1701885ab44f19d8f64f188e6ed4351725a
Gustavo G Herreira <gustavogherreira@gmail.com>
2025-07-27 23:10:08 -0300

    Feat: Implementação do controller
    - Versão do .NET alterada para 9;
    - Setup do NSwag e OpenAPI para teste da API;
    - Implementação de todas as funcionalidades solicitados (CRUD);
    - Criação do "docker-compose.yml" para banco de dados;
    - Modifiquei o provedor de banco de dados de "SqlServer" para "Postgres".

--- Program.cs
+++ Program.cs
@@ -6,22 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument(options =>
+        {
+            // Informações básicas do documento
+            options.DocumentName = "v1";
+            options.Title = "Trilha API Desafio";
+            options.Version = "v1";
+            options.Description = "Aplicação para gerenciar suas tarefas";
+        });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
